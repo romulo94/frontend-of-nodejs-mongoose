249e57d2e8639f4b4c2d51b671fa79dcea936bcb
Romulo Rocha <romulorocha063@gmail.com>
2019-04-15 00:12:20 -0300

    styled-components

--- src/pages/Box/index.js
+++ src/pages/Box/index.js
@@ -8,29 +8,46 @@ import socket from 'socket.io-client';
 import { MdInsertDriveFile } from 'react-icons/md';
 
 import logo from '../../assets/logo.svg';
-import './styles.css';
+import { Container } from './styles'
 
 export default class Box extends Component {
   state = {
-    box: {}
+    box: {},
+    lastRefresh: new Date(),
+    displayRefresh: ''
+
   }
 
   async componentDidMount() {
     this.subscribeToNewFiles();
+    this.lastUpdate();
 
     const box = this.props.match.params.id
     const response = await api.get(`boxes/${box}`);
     this.setState({ box: response.data })
+
+    this.interval = setInterval(() => {
+      this.lastUpdate();
+    }, 5000)
+  }
+
+  lastUpdate = () => {
+    let tempo = distanceInWords(this.state.lastRefresh, new Date(), {
+      locale: pt,
+      includeSeconds: true
+    })
+    this.setState({ displayRefresh: tempo })
   }
 
   subscribeToNewFiles = () => {
     const box = this.props.match.params.id;
     const io = socket('https://omnistack-backend-romulo.herokuapp.com');
-
     io.emit('connectRoom', box);
     io.on('file', data => {
+      this.setState({ lastRefresh: new Date() })
       this.setState({ box: { ...this.state.box, files: [data, ...this.state.box.files] } })
     })
+
   }
 
   handleUpload = files => {
@@ -46,35 +63,39 @@ export default class Box extends Component {
 
   render() {
     return (
-      <div id="box-container">
-        <header>
-          <img src={logo} />
-          <h1>{this.state.box.title}</h1>
-        </header>
-        <Dropzone onDropAccepted={this.handleUpload}>
-          {({ getRootProps, getInputProps }) => (
-            <div className="upload" {...getRootProps()}>
-              <input {...getInputProps()} />
-              <p>Arraste arquivos ou clique aqui</p>
-            </div>
-          )}
-        </Dropzone>
+      <div>
+        <p>última atualização: {this.state.displayRefresh}</p>
+        <Container>
+          <header>
+            <img src={logo} alt='logo' />
+            <h1>{this.state.box.title}</h1>
+          </header>
+          <Dropzone onDropAccepted={this.handleUpload}>
+            {({ getRootProps, getInputProps }) => (
+              <div className="upload" {...getRootProps()}>
+                <input {...getInputProps()} />
+                <p>Arraste arquivos ou clique aqui</p>
+              </div>
+            )}
+          </Dropzone>
 
-        <ul>
-          {
-            this.state.box.files && this.state.box.files.map(file => (
-              <li key={file._id}>
-                <a className="fileInfo" href={file.url}>
-                  <MdInsertDriveFile size={24} color="#A5CFFF" />
-                  <strong>{file.title}</strong>
-                </a>
-                <span>há{" "} {distanceInWords(file.createdAt, new Date(), {
-                  locale: pt
-                })}</span>
-              </li>
-            ))
-          }
-        </ul>
+          <ul>
+            {
+              this.state.box.files && this.state.box.files.map(file => (
+                <li key={file._id}>
+                  <a className="fileInfo" href={file.url}>
+                    <MdInsertDriveFile size={24} color="#A5CFFF" />
+                    <strong>{file.title}</strong>
+                  </a>
+                  <span>há{" "} {distanceInWords(file.createdAt, new Date(), {
+                    locale: pt,
+                    includeSeconds: true
+                  })}</span>
+                </li>
+              ))
+            }
+          </ul>
+        </Container >
       </div>
     );
   }
